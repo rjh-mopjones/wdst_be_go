fd764ea4bf6252f820b012d579d11154419f011b
rjh-mopjones <roryhedderman@gmail.com>
2024-04-26 23:09:34 +0100

    add RSVP struct with create RSVP

--- main.go
+++ main.go
@@ -5,90 +5,44 @@ import (
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
-	"strconv"
 )
 
-type Book struct {
-	ID     int    `json:"id"`
-	Title  string `json:"title"`
-	Author string `json:"author"`
+type RSVP struct {
+	FullName       string `json:"fullName"`
+	Email          string `json:"email"`
+	Starter        string `json:"starter"`
+	Main           string `json:"main"`
+	Dessert        string `json:"dessert"`
+	Song           string `json:"song"`
+	Message        string `json:"message"`
+	Diet           string `json:"diet"`
+	Attendance     bool   `json:"attendance"`
+	AdditionalRSVP []struct {
+		FullName   string `json:"fullName"`
+		Attendance bool   `json:"attendance"`
+		Diet       string `json:"diet"`
+		Starter    string `json:"starter"`
+		Main       string `json:"main"`
+		Dessert    string `json:"dessert"`
+	} `json:"additionalRSVP"`
 }
-
-var books []Book
 
 func main() {
 	// Initialize router
 	router := mux.NewRouter()
 
-	// Dummy data
-	books = append(books, Book{ID: 1, Title: "Book 1", Author: "Author 1"})
-	books = append(books, Book{ID: 2, Title: "Book 2", Author: "Author 2"})
-
 	// Route handlers
-	router.HandleFunc("/books", getBooks).Methods("GET")
-	router.HandleFunc("/books/{id}", getBook).Methods("GET")
-	router.HandleFunc("/books", createBook).Methods("POST")
-	router.HandleFunc("/books/{id}", updateBook).Methods("PUT")
-	router.HandleFunc("/books/{id}", deleteBook).Methods("DELETE")
+	router.HandleFunc("/rsvp", createRSVP).Methods("POST")
 
 	// Start server
+	log.Println("Listening on port 8000")
 	log.Fatal(http.ListenAndServe(":8000", router))
 }
 
-func getBooks(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(books)
-}
-
-func getBook(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	params := mux.Vars(r)
-	for _, item := range books {
-		id, _ := strconv.Atoi(params["id"])
-		if item.ID == id {
-			json.NewEncoder(w).Encode(item)
-			return
-		}
-	}
-	json.NewEncoder(w).Encode(&Book{})
-}
-
-func createBook(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	var book Book
-	_ = json.NewDecoder(r.Body).Decode(&book)
-	book.ID = len(books) + 1
-	books = append(books, book)
-	json.NewEncoder(w).Encode(book)
-}
-
-func updateBook(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	params := mux.Vars(r)
-	id, _ := strconv.Atoi(params["id"])
-	for index, item := range books {
-		if item.ID == id {
-			books = append(books[:index], books[index+1:]...)
-			var book Book
-			_ = json.NewDecoder(r.Body).Decode(&book)
-			book.ID = id
-			books = append(books, book)
-			json.NewEncoder(w).Encode(book)
-			return
-		}
-	}
-	json.NewEncoder(w).Encode(books)
-}
-
-func deleteBook(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	params := mux.Vars(r)
-	id, _ := strconv.Atoi(params["id"])
-	for index, item := range books {
-		if item.ID == id {
-			books = append(books[:index], books[index+1:]...)
-			break
-		}
-	}
-	json.NewEncoder(w).Encode(books)
+func createRSVP(writer http.ResponseWriter, request *http.Request) {
+	writer.Header().Set("Content-Type", "application/json")
+	var rsvp RSVP
+	_ = json.NewDecoder(request.Body).Decode(&rsvp)
+	log.Println(rsvp)
+	json.NewEncoder(writer).Encode(rsvp)
 }
