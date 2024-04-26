96fec7cf3d9236ac611d0d7abcd364c40ac08678
rjh-mopjones <roryhedderman@gmail.com>
2024-04-27 00:42:07 +0100

    add an interface with db

--- main.go
+++ main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
+	"fmt"
 	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
 	"log"
 	"net/http"
 )
 
-type RSVP struct {
+type dtoRsvp struct {
 	FullName       string `json:"fullName"`
 	Email          string `json:"email"`
 	Starter        string `json:"starter"`
@@ -29,20 +32,51 @@ type RSVP struct {
 
 func main() {
 	// Initialize router
+	const (
+		host     = "localhost"
+		port     = 5432
+		user     = ""
+		password = ""
+		dbname   = "wedding"
+	)
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	// Connect to the PostgreSQL database
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	// Verify the connection
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Successfully connected to PostgreSQL!")
+
 	router := mux.NewRouter()
 
-	// Route handlers
-	router.HandleFunc("/rsvp", createRSVP).Methods("POST")
+	router.HandleFunc("/rsvp", createRSVP(db)).Methods("POST")
 
 	// Start server
 	log.Println("Listening on port 8000")
 	log.Fatal(http.ListenAndServe(":8000", router))
 }
 
-func createRSVP(writer http.ResponseWriter, request *http.Request) {
-	writer.Header().Set("Content-Type", "application/json")
-	var rsvp RSVP
-	_ = json.NewDecoder(request.Body).Decode(&rsvp)
-	log.Println(rsvp)
-	json.NewEncoder(writer).Encode(rsvp)
+func createRSVP(db *sql.DB) func(writer http.ResponseWriter, request *http.Request) {
+	return func(writer http.ResponseWriter, request *http.Request) {
+		writer.Header().Set("Content-Type", "application/json")
+		sqlStatement := "INSERT INTO rsvp (first_name) VALUES ($1) RETURNING id"
+		var rsvp dtoRsvp
+		var id int
+		_ = json.NewDecoder(request.Body).Decode(&rsvp)
+		err := db.QueryRow(sqlStatement, rsvp.FullName).Scan(&id)
+		if err != nil {
+			log.Fatal(err)
+		}
+		_ = json.NewEncoder(writer).Encode(id)
+	}
 }
